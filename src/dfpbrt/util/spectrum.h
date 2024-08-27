598b45dcf76ee0d7e5d13c1ef0973c2a76a90d0f
gmf128 <orlando128@163.com>
2024-08-27 10:41:41 +0800

    chore(dfpbrt): start work in taggedptr and spectrum

---
+++ src/dfpbrt/util/spectrum.h
@@ -0,0 +1,19 @@
+#ifndef DFPBRT_UTIL_SPECTRUM_H
+#define DFPBRT_UTIL_SPECTRUM_H
+
+#include <dfpbrt/dfpbrt.h>
+#include <dfpbrt/util/vecmath.h>
+
+
+namespace dfpbrt{
+
+// The range of visible range
+constexpr Float Lambda_min = 360, Lambda_max = 830;
+
+
+
+
+}
+
+
+#endif
