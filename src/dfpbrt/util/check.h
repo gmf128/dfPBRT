4cdc12832a0c9a2ef45aa0a2750aa90c646cd173
gmf128 <orlando128@163.com>
2024-08-27 10:59:53 +0800

    feat(check): add more check macros

--- src/dfpbrt/util/check.h
+++ src/dfpbrt/util/check.h
@@ -11,7 +11,32 @@ namespace dfpbrt{
     //(LOG_FATAL(...), true) is a comma expression, whose value is true(the latter one)
     //TODO: #define CHECK(x) (!(!(x) && (LOG_FATAL("Check failed: %s", #x), true)))
   #define CHECK(x) (!(!(x) && (LOG_FATAL(std::format("Check failed {:s}", #x)), true)))
-    #define DCHECK(x) CHECK(x)
+  #define CHECK_EQ(a, b) CHECK_IMPL(a, b, ==)
+  #define CHECK_NE(a, b) CHECK_IMPL(a, b, !=)
+  #define CHECK_GT(a, b) CHECK_IMPL(a, b, >)
+  #define CHECK_GE(a, b) CHECK_IMPL(a, b, >=)
+  #define CHECK_LT(a, b) CHECK_IMPL(a, b, <)
+  #define CHECK_LE(a, b) CHECK_IMPL(a, b, <=)
+
+  // CHECK\_IMPL Macro Definition
+#define CHECK_IMPL(a, b, op)                                                           \
+    do {                                                                               \
+        auto va = a;                                                                   \
+        auto vb = b;                                                                   \
+        if (!(va op vb))                                                               \
+            LOG_FATAL("Check failed: %s " #op " %s with %s = %s, %s = %s", #a, #b, #a, \
+                      va, #b, vb);                                                     \
+    } while (false) /* swallow semicolon */
+
+#endif 
+
+  #define DCHECK(x) (CHECK(x))
+  #define DCHECK_EQ(a, b) CHECK_EQ(a, b)
+  #define DCHECK_NE(a, b) CHECK_NE(a, b)
+  #define DCHECK_GT(a, b) CHECK_GT(a, b)
+  #define DCHECK_GE(a, b) CHECK_GE(a, b)
+  #define DCHECK_LT(a, b) CHECK_LT(a, b)
+  #define DCHECK_LE(a, b) CHECK_LE(a, b)
 
     // CheckCallbackScope Definition
 class CheckCallbackScope {
